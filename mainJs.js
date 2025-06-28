9a5de3067967f229c3f62d8f2298fd1831a13324
prachi-max <prachi.nawale09@gmail.com>
2025-06-28 18:34:19 +0530

    upload

---
+++ mainJs.js
@@ -0,0 +1,7 @@
+// document.querySelector('a[tag="html_skill"]').addEventListener("mouseover", function () {
+//     document.getElementById("skill_html").style.display = "unset";
+// });
+
+// document.querySelector('a[tag="html_skill"]').addEventListener("mouseout", function () {
+//     document.getElementById("skill_html").style.display = "none";
+// });
